356a119f07b37c95220ae94144b0d66e223fe3e6
Sunday Ochuko <sundayochuko101@gmail.com>
2025-08-20 22:30:28 +0100

    updated the profile responsiveness

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,10 +1,47 @@
 import React from "react";
-import ProfileSection from "../../components/profile/profileSection";
+import { ImPencil } from "react-icons/im";
+import { FaPlus } from "react-icons/fa6";
 
 const page = () => {
   return (
-    <div>
-      <ProfileSection />
+    <div className="min-h-screen h-auto w-full flex justify-center items-center ">
+      <div className=" h-[50%] w-[70%] flex flex-col">
+        <h1 className="text-center text-4xl font-bold text-highlight m-6">
+          Who Is Watching?
+        </h1>
+        <div className="h-full w-full grid grid-cols-2 lg:grid-cols-4  gap-2 mt-6">
+          <div className=" h-auto w-auto  flex flex-col items-center">
+            <div className="relative h-[180px] w-[180px] rounded-full bg-sky-400 ">
+              <div className="absolute right-7 lg:right-0 bottom-0 h-10 w-10 flex items-center justify-center text-black text-[12px] bg-white rounded-full">
+                <ImPencil />
+              </div>
+            </div>
+            <h2 className="font-bold mt-3">David</h2>
+          </div>
+          <div className=" h-auto w-auto   flex flex-col items-center">
+            <div className=" relative h-[180px] w-[180px] rounded-full bg-amber-700 ">
+              <div className="absolute right-7 lg:right-0 bottom-0 h-10 w-10 flex items-center justify-center text-black text-[12px] bg-white rounded-full">
+                <ImPencil />
+              </div>
+            </div>
+            <h2 className="  font-bold mt-3">Denisa</h2>
+          </div>
+          <div className=" h-auto w-auto flex flex-col items-center">
+            <div className=" relative h-[180px] w-[180px] rounded-full bg-pink-200 ">
+              <div className="absolute right-7 lg:right-0 bottom-0 h-10 w-10 flex items-center justify-center text-black text-[12px] bg-white rounded-full">
+                <ImPencil />
+              </div>
+            </div>
+            <h2 className="font-bold mt-3">Johny</h2>
+          </div>
+          <div className="h-auto w-auto flex flex-col items-center">
+            <div className=" h-[180px]  w-[180px] flex items-center justify-center rounded-full border-2 border-highlight">
+              <FaPlus className="text-highlight" />
+            </div>
+            <h2 className="font-bold mt-3">Add Profile</h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,22 +3,7 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}", // adjust based on your project structure
   ],
   theme: {
-    extend: {
-      // screens: {
-      //   'xs': '320px',   // small phones
-      //   'sm': '480px',   // larger phones
-      //   'md': '640px',   // tablets
-      //   'lg': '768px',   // small laptops
-      //   'xl': '1024px',  // desktops
-      //   '2xl': '1280px', // large desktops
-      //   '3xl': '1536px', // 2K screens
-      // },
-      // spacing: {
-      //   'swiper-gap-sm': '5px',   // minimal gap
-      //   'swiper-gap-md': '10px',  // medium gap
-      //   'swiper-gap-lg': '15px',  // larger gap
-      // }
-    },
+    extend: {},
   },
   plugins: [require("tailwind-scrollbar-hide")],
 };
