92b7cfb3a247c04b58787a1e21290cebf04dd57f
Adedeji John Abidemi <adedejijohn099@gmail.com>
2025-09-11 08:56:27 -0500

    force git to register the rename

--- app/movies/page.jsx
+++ app/movies/page.jsx
@@ -4,10 +4,6 @@ import React, { useEffect } from "react";
 import Header from "@/components/movies/header";
 import MainSection from "@/components/movies/mainSection";
 import { useTMBD } from "@/contexts/TMDBContext";
-// import axios from "axios";
-
-// const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
-// const BASE_URL = "https://api.themoviedb.org/3";
 
 function page() {
   const { similarMoviesData } = useTMBD();
